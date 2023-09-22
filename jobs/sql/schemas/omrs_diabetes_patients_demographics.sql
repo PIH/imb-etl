4e27b429bf1859d2fd97e5a2cc6092916793d284
Michael Seaton <mseaton@pih.org>
2023-09-22 15:20:06 -0400

    Move all pentaho-related jobs under the pentaho folder.  Add new folder for sql jobs.

---
+++ jobs/sql/schemas/omrs_diabetes_patients_demographics.sql
@@ -0,0 +1,20 @@
+CREATE TABLE omrs_diabetes_patients_demographics (
+  patient_id int,
+  Medical_Record_Number varchar(50),
+  Patient_Names varchar(152),
+  Age bigint,
+  Gender varchar(50),
+  country varchar(50),
+  state_province varchar(255),
+  district varchar(255),
+  sector varchar(255),
+  cell varchar(255),
+  umudugudu varchar(255),
+  health_facility varchar(255),
+  Patient_status varchar(5),
+  Enrolment_Date datetime,
+  Completed_Date datetime,
+  Diabetes_treatment_status varchar(255),
+  Monitoring_status varchar(255),
+  Home_glucometer_study varchar(255)  
+);
